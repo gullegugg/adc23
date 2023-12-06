f4463b947592d6a68d2fcb4fbbc6e746b853b96e
Gustav Hagland <gustav.hagland@gmail.com>
2023-12-06 11:05:50 +0100

    part2!

--- src/day4.rs
+++ src/day4.rs
@@ -1,26 +1,22 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use crate::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Card {
     winning_numbers: Vec<u32>,
     playing_numbers: Vec<u32>,
 }
 
 impl Card {
-    fn worth(&self) -> u32 {
+    fn winning_count(&self) -> u32 {
         let winning_count = self
             .playing_numbers
             .iter()
             .filter(|num| self.winning_numbers.contains(num))
             .count();
 
-        if winning_count == 0 {
-            return 0;
-        }
-
-        2_u32.pow(winning_count as u32 - 1)
+        winning_count as u32
     }
 }
 
@@ -63,16 +59,47 @@ impl FromStr for Card {
     }
 }
 
-pub fn sum_winning_cards<T: Iterator<Item = Result<String, std::io::Error>>>(
+fn sum_winning_cards<T: Iterator<Item = Result<String, std::io::Error>>>(
     input: T,
 ) -> anyhow::Result<u32> {
     let mut sum = 0;
     for line in input {
         let card: Card = line?.parse()?;
-        sum += card.worth();
+        let winning_count = card.winning_count();
+        if winning_count > 0 {
+            sum += 2_u32.pow(winning_count - 1)
+        }
     }
 
     Ok(sum)
+}
+
+fn count_winning_cards<T: Iterator<Item = Result<String, std::io::Error>>>(
+    input: T,
+) -> anyhow::Result<u32> {
+    let mut card_amount_map: HashMap<usize, u32> = HashMap::new();
+    let mut num_cards = 0;
+    for (i, line) in input.enumerate() {
+        let card: Card = line?.parse()?;
+        let winning_count = card.winning_count();
+        let card_count = card_amount_map.get(&i).unwrap_or(&0) + 1;
+
+        card_amount_map.insert(i, card_count);
+
+        for winning_offset in 1..winning_count + 1 {
+            let index_to_update = i + winning_offset as usize;
+            let current_count = card_amount_map.get(&index_to_update).unwrap_or(&0);
+
+            card_amount_map.insert(index_to_update, current_count + card_count);
+        }
+        num_cards += 1;
+    }
+
+    Ok(card_amount_map
+        .iter()
+        .filter(|(i, _)| **i < num_cards as usize)
+        .map(|(_, v)| v)
+        .sum())
 }
 
 pub fn challenge<T: Iterator<Item = Result<String, std::io::Error>>>(
@@ -81,6 +108,7 @@ pub fn challenge<T: Iterator<Item = Result<String, std::io::Error>>>(
 ) -> anyhow::Result<u32> {
     match part {
         1 => sum_winning_cards(input),
+        2 => count_winning_cards(input),
         _ => Ok(0),
     }
 }
@@ -109,5 +137,27 @@ mod tests {
 
         // Then
         assert_eq!(13, sum);
+    }
+
+    #[test]
+    fn part2_example() {
+        // Given
+        let binding = vec![
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        ];
+        let input = binding
+            .iter()
+            .map(|it| Ok::<_, std::io::Error>(it.to_string()));
+
+        // When
+        let sum = challenge(2, input).unwrap();
+
+        // Then
+        assert_eq!(30, sum);
     }
 }
